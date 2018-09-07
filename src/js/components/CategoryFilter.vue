69e17a84f9d10ab35589a9c81e5c451f9cfc05f3
Jakubem <jakubmx@gmail.com>
2018-09-07 18:40:28 +0200

    add filtering reset

--- src/js/components/CategoryFilter.vue
+++ src/js/components/CategoryFilter.vue
@@ -8,7 +8,7 @@
         <label class="filter-radio-label" 
           v-for="el in categories" 
           :key="el.lenght">
-          <input tabindex="1" class="filter-radio"
+          <input class="filter-radio"
             :ref='el' 
             :class="el" 
             type="radio" 
@@ -19,6 +19,11 @@
               {{ el }}
             </span>
         </label>
+        <button 
+          v-if="pickedEl !== ''" 
+          class="filter-reset CTA" 
+          @click="resetFilter">reset
+        </button>
       </div>
     </fieldset>
   </section>
@@ -32,8 +37,16 @@
       }
     },
     mounted: function() {
+      console.table(this.pickedEl);
       const currentCategory = this.$route.query.category;
-      this.pickedEl = currentCategory;
+      this.pickedEl = currentCategory || '';
+    },
+    methods: {
+      resetFilter: function () {
+        this.pickedEl = '';
+        window.location.href = `/#/wszystko`;
+        this.$root.$emit('reset', '');
+      }
     },
     computed: {
       picked: {
@@ -55,6 +68,9 @@
   @import '../../css/_var.scss';
   .filter-wrapper {
     margin: 50px 0;
+    width: 100%;
+    display: flex;
+    justify-content: center;
   }
 
   .filter-fieldset {
@@ -65,42 +81,53 @@
 
   .filter-legend {
     font: $f-h2;
-    margin: 0 0 0 5px;
+    margin: 0 0 10px 5px;
     padding: 0;
   }
 
   .filter-radio {
-    // -webkit-appearance: none;
-    // -moz-appearance: none;
-    // appearance: none;
-    margin: 20px 0;
-    padding: 0;
-    opacity: 0;
+    position: absolute !important;
+    top: -9999px !important;
+    left: -9999px !important;
   }
   
   .filter-radio-label {
-    margin: 0;
-    padding: 0;
-    display: inline;
+    padding: 10px 0;
+    margin: 5px 0;
+    height: 100%;
   }
+
   .filter-radio-wrapper {
     display: flex;
     flex-direction: row;
     flex-wrap: wrap;
+    align-items: center;
+  }
+
+  .filter-reset {
+    border: none;
+    padding: 5px 10px;
+    margin: 5px;
+    box-sizing: border-box;
+    font: $f-h2;
+    background: $c-red;
   }
 
   .filter-radio-label .filter-radio-content {
     font: $f-h2;
     text-align: center;
     background-color: $c-yellow;
-    padding: 5px 15px;
-    margin: 5px;
+    padding: 5px 10px;
+    margin: 0 5px;
+    box-sizing: border-box;
+    height: 100%;
     color: $c-black;
     border-radius: $b-radius;
   }
 
   .filter-radio:checked + .filter-radio-content {
     background-color: $c-green;
+    box-shadow: $Shadow-base;
   }
   .filter-radio:focus + .filter-radio-content {
     background-color: $c-orange;
@@ -114,7 +141,6 @@
     .filter-radio-wrapper {
       justify-content: center;
     }
-
 }
 
 </style>
