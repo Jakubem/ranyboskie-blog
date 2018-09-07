fd019ad16f7bf8946df19b6198300c8ae6cfa617
Jakubem <jakubmx@gmail.com>
2018-09-08 00:40:49 +0200

    add responsive hamburger menu

--- src/js/components/HeaderNav.vue
+++ src/js/components/HeaderNav.vue
@@ -1,17 +1,18 @@
 <template>
   <header class="header">
-    <router-link tag="h1" to="/">
-      <span class="header-logo">
+    <router-link class="header-logo" tag="h1" to="/">
         Ranyboskie
-      </span>
     </router-link>
-    <nav>
-      <ul class="header-ul">
-        <!-- <li class="header-li">
+    <nav class="header-nav">
+      <button @click="menuOpen = !menuOpen" class="hamburger">
+        <i-hamburger></i-hamburger>
+      </button>
+      <ul class="header-ul" :class="{open: menuOpen}">
+        <li class="header-li">
           <router-link tag="a" to="/fajne-rzeczy" class="header-link" aria-label="zobacz fajne rzeczy">
             fajne rzeczy
           </router-link>
-        </li> -->
+        </li>
         <li class="header-li">
           <router-link tag="a" to="/wszystko" class="header-link" aria-label="zobacz wszystko">
             wszystko
@@ -41,16 +42,24 @@
   import iGithub from './icons/i-github.vue' 
   import iInstagram from './icons/i-instagram.vue' 
   import iTwitter from './icons/i-twitter.vue' 
+  import iHamburger from './icons/i-hamburger.vue' 
   export default {
     components: {
       iGithub,
       iInstagram,
-      iTwitter
+      iTwitter,
+      iHamburger,
     },
      data () {
       return {
+        menuOpen: false
       }
     },
+    methods: {
+      // openMenu: function() {
+        // this.menuOpen = 
+      // }
+    }
   }
 </script>
 
@@ -62,7 +71,7 @@
     position: sticky;
     top: 0;
     height: 40px;
-    padding: 3px;
+    padding: 0 5px;
     box-sizing: border-box;
     display: flex;
     justify-content: space-between;
@@ -75,22 +84,25 @@
     display: flex;
     align-items: center;
     padding: 0;
-    margin-right: 0;
+    margin: 0;
+    box-sizing: border-box;
   }
 
   .header-li {
     list-style: none;
-    margin-left: 0;
-    padding: 10px;
-    border-collapse: collapse;
+    margin: 0 5px;
+    padding: 0;
+    max-height: 40px;
+    box-sizing: border-box;
+    // border-collapse: collapse;
   }
 
   .header-logo {
     font: $f-h2-logo;
     color: #fff;
     letter-spacing: $f-l-spacing-hi;
-    margin-left: 4px;
-    transform: rotate(-1.76deg);
+    margin: 0;
+    transform: rotate(-1.5deg);
     text-shadow:
       -3px 5px 0px $c-red-dimmed, 
       5px -4px 0px $c-aqua-dimmed, 
@@ -106,12 +118,21 @@
     padding: 0;
     margin: 0;
     color: $c-white;
+    box-sizing: border-box;
     letter-spacing: $f-l-spacing;
     &:hover {
       text-decoration: underline;
     }
   }
-  @media screen and (max-width: 500px){
+
+  .hamburger {
+    display: none;
+    background: none;
+    border: none;
+    margin-right: 5px;
+  }
+
+  @media screen and (max-width: 600px){
     .header-logo {
       font: $f-h2-logo-mob;
       margin: 0;
@@ -120,7 +141,35 @@
       font: $f-link-mob;
     }
     .header-li {
-      padding: 3px;
+      padding: 15px;
+    }
+  }
+  @media screen and (max-width: 500px){
+    .hamburger {
+      display: block;
+    }
+    .header-nav {
+      display: flex;
+      flex-direction: row;
+    }
+    .header-li {
+      padding: 10px;
+    }
+    .header-ul {
+      position: absolute;
+      top: -100px;
+      right: 100px;
+      // overflow: hidden;
+    }
+    .open {
+      flex-direction: column;
+      justify-content: flex-end;
+      background: $c-purple;
+      top: 0;
+      right: 0;
+      padding: 40px 0 10px 0;
+      z-index: -1;
+      border-radius: 0 0 0 25px;
     }
   }
 </style>
